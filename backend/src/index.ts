8f1669951506dc16692fda4efca8f1d916a673bf
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-07-25 14:06:12 +0100

    implement backend user model, route and countroller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/myUserRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
 
+app.use(express.json());
+
+
+
+app.use("/api/my/user", myUserRoute);
 
 app.get("/test", async(req: Request, res: Response) => {
   res.json({ message: "Hello"});
