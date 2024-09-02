120d22d1f8c53eeae385e3ecab153906eebc51c9
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-09-02 20:40:22 +0100

    modify tsconfig.json

--- backend/src/routes/myUserRoute.ts
+++ backend/src/routes/myUserRoute.ts
@@ -1,3 +1,4 @@
+
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
@@ -7,17 +8,13 @@ const router = express.Router();
 
 // /api/my/user
 router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
-router.post(
-  "/", jwtCheck,
-  MyUserController.createCurrentUser
-);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put(
-  "/", 
-  jwtCheck, 
-  jwtParse, 
-  validateMyUserRequest, 
-  MyUserController.updateCurrentUser)
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;
-
-// export const myUserRoute = router;
