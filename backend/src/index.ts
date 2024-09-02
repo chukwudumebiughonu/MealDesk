7807df00142473691e45069b084b99305a357b2a
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-09-02 15:27:15 +0100

    Fix TypeScript configuration and file imports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
 
 const app = express();
