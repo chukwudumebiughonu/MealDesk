6243c492044adc812512b7afee2d28fd41e21b58
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-09-02 20:57:46 +0100

    Update TypeScript configuration and import statements

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute.js";
 // import { myUserRoute } from "./routes/MyUserRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
